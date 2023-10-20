dce46536055a4a946c0994ed81f4fb0591d08197
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2023-10-20 18:03:06 +0530

    new event controller

--- src/event.controller.ts
+++ src/event.controller.ts
@@ -1,15 +1,31 @@
-import { Controller, Get, Post, Patch, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
 
 @Controller('/event')
 export class EventController {
   @Get()
   findAll() {}
-  @Get()
-  findOne() {}
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return id;
+  }
   @Post()
-  create() {}
-  @Patch()
-  update() {}
-  @Delete()
-  delete() {}
+  create(@Body() body: any) {
+    return body;
+  }
+  @Patch(':id')
+  update(@Param('id') id: string) {
+    return id;
+  }
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return id;
+  }
 }
